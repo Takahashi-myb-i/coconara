b92f4358fb271dac26bac52fcbf4392d5c7c8791
takahashi-miyabi <154243176+Takahashi-myb-i@users.noreply.github.com>
2024-05-21 08:54:04 +0900

    Add files via upload

---
+++ coconara_unelmasama/js/4-1-4.js
@@ -0,0 +1,8 @@
+
+$(window).on('load', function() {
+
+    $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+    $("#splash-logo").delay(1000).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述   
+
+  });
+  
